4d9dae2214fec84d8f7509604ff9cb63e2ef6d77
Antoni Wittstock <antoni.wittstock@icloud.com>
2025-01-09 11:19:06 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Program
+{
+    class Zadanie
+    {
+        
+        class Film
+        {
+            protected string tytul;
+            protected int liczbaWypozyczen;
+
+            public Film()
+            {
+                this.tytul = string.Empty;
+                this.liczbaWypozyczen = 0;
+            }
+            public void SetTytul(string tytuluzytkownika)
+            {
+                tytul = tytuluzytkownika;
+            }
+
+            public string GetTytul()
+            {
+                return tytul;
+            }
+            public int GetWypozyczenia()
+            {
+                return liczbaWypozyczen;
+            }
+            public void PrzechowujWypozyczenia()
+            {
+                liczbaWypozyczen++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Film film = new Film();
+            film.SetTytul("Mojfilmik");
+            Console.WriteLine(film.GetTytul());
+            Console.WriteLine(film.GetWypozyczenia());
+            film.PrzechowujWypozyczenia();
+            Console.WriteLine(film.GetWypozyczenia());
+        }
+    }
+}
